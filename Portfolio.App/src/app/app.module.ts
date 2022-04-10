818f1cefceeffb9aadea035884f4474816fc4a7a
Pawel Maslak <maslak.pawel89@gmail.com>
2022-04-11 00:36:38 +0200

    Added basic components and remove angular routing.

--- Portfolio.App/src/app/app.module.ts
+++ Portfolio.App/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { MyProjectsComponent } from './my-projects/my-projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopMenuComponent,
+    AboutMeComponent,
+    WorkExperienceComponent,
+    MyProjectsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
