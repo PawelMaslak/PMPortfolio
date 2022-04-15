c8eeb90f50d3824fc7bff5bc2139b320882ec608
Pawel Maslak <maslak.pawel89@gmail.com>
2022-04-15 11:59:42 +0200

    Added main content component

--- Portfolio.App/src/app/app.module.ts
+++ Portfolio.App/src/app/app.module.ts
@@ -3,17 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TopMenuComponent } from './top-menu/top-menu.component';
-import { AboutMeComponent } from './about-me/about-me.component';
-import { WorkExperienceComponent } from './work-experience/work-experience.component';
-import { MyProjectsComponent } from './my-projects/my-projects.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopMenuComponent,
-    AboutMeComponent,
-    WorkExperienceComponent,
-    MyProjectsComponent
+    ContentComponent
   ],
   imports: [
     BrowserModule,
--- Portfolio.App/src/app/content/content.component.spec.ts
+++ Portfolio.App/src/app/content/content.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MyProjectsComponent } from './my-projects.component';
+import { ContentComponent } from './content.component';
 
-describe('MyProjectsComponent', () => {
-  let component: MyProjectsComponent;
-  let fixture: ComponentFixture<MyProjectsComponent>;
+describe('ContentComponent', () => {
+  let component: ContentComponent;
+  let fixture: ComponentFixture<ContentComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MyProjectsComponent ]
+      declarations: [ ContentComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(MyProjectsComponent);
+    fixture = TestBed.createComponent(ContentComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
