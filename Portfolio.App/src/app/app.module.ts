db5e7300385a2a95d35e04eed9e93b5c29b8e298
Pawel Maslak <maslak.pawel89@gmail.com>
2022-04-15 12:01:41 +0200

    Added main content sections components

--- Portfolio.App/src/app/app.module.ts
+++ Portfolio.App/src/app/app.module.ts
@@ -4,12 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TopMenuComponent } from './top-menu/top-menu.component';
 import { ContentComponent } from './content/content.component';
+import { AboutComponent } from './content/about/about.component';
+import { WorkComponent } from './content/work/work.component';
+import { ProjectsComponent } from './content/projects/projects.component';
+import { ContactComponent } from './content/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopMenuComponent,
-    ContentComponent
+    ContentComponent,
+    AboutComponent,
+    WorkComponent,
+    ProjectsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
