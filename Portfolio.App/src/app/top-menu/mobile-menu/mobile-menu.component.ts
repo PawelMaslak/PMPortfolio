8de443c9ee3c3f42b1a61950afbb8bb954f356c8
Pawel Maslak <maslak.pawel89@gmail.com>
2022-08-18 18:54:41 +0200

    Added bars and xbars to mobile menu

--- Portfolio.App/src/app/top-menu/mobile-menu/mobile-menu.component.ts
+++ Portfolio.App/src/app/top-menu/mobile-menu/mobile-menu.component.ts
@@ -12,25 +12,20 @@ export class MobileMenuComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  open() {
+  clickMenu() {
+    let hamburger = document.getElementById("hamburger-1");
     let links = document.getElementById("links");
-    let button = document.getElementById("button");
-    let bars = document.getElementById("bars");
-    let close = document.getElementById("close");
+    hamburger?.classList.toggle("is-active");
 
     if (links!.style.opacity === "1") {
       links!.style.opacity = "0";
       links!.style.height = "0px";
-      button!.classList.remove("active");
-      //close!.style.display = "none";
-      //bars!.style.display = "block";
     }
     else {
       links!.style.opacity = "1";
       links!.style.height = "100%";
-      button!.classList.add("active");
-      //bars!.style.display = "none";
-      //close!.style.display = "block";
     }
   }
+
+  //Add method to close menu on clicking link.
 }
