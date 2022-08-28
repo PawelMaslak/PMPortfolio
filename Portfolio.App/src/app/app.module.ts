3bc1386def93d1caf281fd3fb7f72be02b25fe7c
Pawel Maslak <maslak.pawel89@gmail.com>
2022-08-28 18:00:43 +0200

    Updated projects.scss to be fit for mobile devices.

--- Portfolio.App/src/app/app.module.ts
+++ Portfolio.App/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TopMenuComponent } from './top-menu/top-menu.component';
 import { ContentComponent } from './content/content.component';
-import { AboutComponent } from './content/about/about.component';
 import { ProjectsComponent } from './content/projects/projects.component';
 import { ContactComponent } from './content/contact/contact.component';
 import { FooterComponent } from './content/footer/footer.component';
@@ -31,7 +30,6 @@ import { DefaultMenuComponent } from './top-menu/default-menu/default-menu.compo
     AppComponent,
     TopMenuComponent,
     ContentComponent,
-    AboutComponent,
     ProjectsComponent,
     ContactComponent,
     FooterComponent,
